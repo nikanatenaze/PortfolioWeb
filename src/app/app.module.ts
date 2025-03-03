e8920d5bf80ce493fecf2ddcafbc5ecdf9cc6f04
Nika Natenadze <nikanatenaze@gmail.com>
2025-03-04 03:46:04 +0400

    Adding home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ErrorComponent } from './pages/error/error.component';
+import { HomeStartComponent } from './pages/home/HomeComponents/Start/home-start/home-start.component'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,11 +20,13 @@ import { ErrorComponent } from './pages/error/error.component';
     HomeComponent,
     AboutComponent,
     ProjectsComponent,
-    ErrorComponent
+    ErrorComponent,
+    HomeStartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/GitHub/git-hub.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GitHubService {
+
+  constructor(public api: HttpClient) { }
+
+  getRepositories() {
+    return this.api.get("https://api.github.com/users/nikanatenaze/repos");
+  }
+
+  getRepositoryByName(name:string) {
+    return this.api.get(`https://api.github.com/repos/nikanatenaze/${name}`)
+  }
+
+  getMyData() {
+    return this.api.get(`https://api.github.com/users/nikanatenaze`)
+  }
+}
