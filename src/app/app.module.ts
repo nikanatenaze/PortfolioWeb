c2655bd0993aae580d0cd3ace1290bbb6ce7d971
Nika Natenadze <nikanatenaze@gmail.com>
2025-03-08 19:29:47 +0400

    File management update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './importantparts/navigation/navigation.component';
-import { FooterComponent } from './importantparts/footer/footer.component';
+import { NavigationComponent } from './important-parts/navigation/navigation.component';
+import { FooterComponent } from './important-parts/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ErrorComponent } from './pages/error/error.component';
-import { HomeStartComponent } from './pages/home/HomeComponents/Start/home-start/home-start.component'
+import { HomeStartComponent } from './pages/home/home-components/start/home-start/home-start.component'
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { HomeAboutComponent } from './pages/home/homecomponents/about/home-about/home-about.component';
-import { HomeFeaturedComponent } from './pages/home/homecomponents/featured/home-featured/home-featured.component';
+import { HomeAboutComponent } from './pages/home/home-components/about/home-about/home-about.component';
+import { HomeFeaturedComponent } from './pages/home/home-components/featured/home-featured/home-featured.component';
 
 @NgModule({
   declarations: [
