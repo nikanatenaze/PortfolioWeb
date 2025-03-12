78753274385a4caa04a555f4a697b513eaa295ef
Nika Natenadze <nikanatenaze@gmail.com>
2025-03-12 19:22:35 +0400

    Responsive Navigation

---
+++ src/app/important-parts/navigation/hidden-navigation/hidden-navigation/hidden-navigation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hidden-navigation',
+  standalone: false,
+  templateUrl: './hidden-navigation.component.html',
+  styleUrl: './hidden-navigation.component.css'
+})
+export class HiddenNavigationComponent {
+
+}
